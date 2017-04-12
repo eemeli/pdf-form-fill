0f7481866e4416f22116aec6721ff48d788e4be2
Eemeli Aro <eemeli@gmail.com>
2017-04-13 01:40:43 +0300

    Add jsdoc comments

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const fs = require('fs')
 const tmp = require('tmp')
 const xfdf = require('xfdf')
 
+
+/** Display the available fields of a PDF form
+ *
+ * @param {string} pdf - path for the source PDF file
+ * @returns {Promise(Object)} provides the map of field names to their attributes and values
+ */
 function fields (pdf) {
   return new Promise((resolve, reject) => {
     const pdftk = spawn('pdftk', [pdf, 'dump_data_fields_utf8'])
@@ -35,6 +41,25 @@ function fields (pdf) {
   })
 }
 
+
+/** Fill a PDF form with data
+ *
+ * @param {string} pdf - path for the source PDF file
+ * @param {Object.<string,string>} fields - a flat map of data to populate the form's fields
+ * @param {?Object} options - customise the output; currently only supports `flatten`, default true
+ * @returns {Promise(stream.Readable)} provides the output PDF
+ *
+ * @example
+ * const fs = require('fs')
+ * const srcPdf = '...'
+ * const tgtPdf = '...'
+ * const data = { name1: 'Value 1', checkbox2: 'Yes' }
+ *
+ * const output = fs.createWriteStream(tgtPdf)
+ * fill(scrPdf, data)
+ *   .then(stream => stream.pipe(output))
+ *   .catch(err => console.error(err))
+ */
 function fill (pdf, fields, options = {}) {
   const { flatten = true } = options
   const xfdfBuilder = new xfdf({ pdf })
