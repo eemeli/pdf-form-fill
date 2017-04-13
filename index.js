63444febe5a443f0f0d4b306e2704ed58f3a5057
Eemeli Aro <eemeli@gmail.com>
2017-04-13 18:44:29 +0300

    Check input path with fs.access

--- index.js
+++ index.js
@@ -11,32 +11,35 @@ const xfdf = require('xfdf')
  */
 function fields (pdf) {
   return new Promise((resolve, reject) => {
-    const { stderr, stdout } = spawn('pdftk', [pdf, 'dump_data_fields_utf8'])
-    let output = ''
-    stderr.on('data', (err) => reject(new Error(err.toString())))
-    stdout.on('data', (chunk) => { output += chunk })
-    stdout.on('end', () => {
-      const result = {}
-      output.split('---').forEach(field => {
-        const data = {}
-        const re = /^Field([a-z]+): (.*)/gim
-        let match
-        while ((match = re.exec(field)) !== null) {
-          const [_, key, value] = match
-          switch (key) {
-            case 'Name':
-              result[value] = data
-              break
-            case 'StateOption':
-              if (!data.options) data.options = [value]
-              else data.options.push(value)
-              break
-            default:
-              data[key.toLowerCase()] = value
+    fs.access(pdf, fs.constants.R_OK, (err) => {
+      if (err) return reject(err)
+      const { stderr, stdout } = spawn('pdftk', [pdf, 'dump_data_fields_utf8'])
+      let output = ''
+      stderr.on('data', (err) => reject(new Error(err.toString())))
+      stdout.on('data', (chunk) => { output += chunk })
+      stdout.on('end', () => {
+        const result = {}
+        output.split('---').forEach(field => {
+          const data = {}
+          const re = /^Field([a-z]+): (.*)/gim
+          let match
+          while ((match = re.exec(field)) !== null) {
+            const [_, key, value] = match
+            switch (key) {
+              case 'Name':
+                result[value] = data
+                break
+              case 'StateOption':
+                if (!data.options) data.options = [value]
+                else data.options.push(value)
+                break
+              default:
+                data[key.toLowerCase()] = value
+            }
           }
-        }
+        })
+        resolve(result)
       })
-      resolve(result)
     })
   })
 }
@@ -65,22 +68,25 @@ function fill (pdf, fields, options = {}) {
   const xfdfBuilder = new xfdf({ pdf })
   xfdfBuilder.fromJSON({ fields })
   return new Promise((resolve, reject) => {
-    tmp.file((err, path, fd) => {
+    fs.access(pdf, fs.constants.R_OK, (err) => {
       if (err) return reject(err)
-      fs.write(fd, xfdfBuilder.generate(), (err, written) => {
+      tmp.file((err, path, fd) => {
         if (err) return reject(err)
-        if (written === 0) return reject(new Error('xfdf wrote 0 bytes!'))
-        const args = [pdf, 'fill_form', path, 'output', '-']
-        if (flatten) args.push('flatten')
-        const { stderr, stdout } = spawn('pdftk', args)
-        const listener = (data) => {
-          stdout.pause()
-          stdout.unshift(data)
-          resolve(stdout)
-          stdout.removeListener('data', listener)
-        }
-        stderr.on('data', (err) => reject(new Error(err.toString())))
-        stdout.on('data', listener)
+        fs.write(fd, xfdfBuilder.generate(), (err, written) => {
+          if (err) return reject(err)
+          if (written === 0) return reject(new Error('xfdf wrote 0 bytes!'))
+          const args = [pdf, 'fill_form', path, 'output', '-']
+          if (flatten) args.push('flatten')
+          const { stderr, stdout } = spawn('pdftk', args)
+          const listener = (data) => {
+            stdout.pause()
+            stdout.unshift(data)
+            resolve(stdout)
+            stdout.removeListener('data', listener)
+          }
+          stderr.on('data', (err) => reject(new Error(err.toString())))
+          stdout.on('data', listener)
+        })
       })
     })
   })
