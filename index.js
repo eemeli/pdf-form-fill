43c67de13b1de5a1c2411a19bb5dee360640292a
Eemeli Aro <eemeli@gmail.com>
2017-04-14 14:45:53 +0300

    Set info fields in output PDF

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const spawn = require('child_process').spawn
 const fs = require('fs')
 const tmp = require('tmp')
-const xfdf = require('xfdf')
+const XFDF = require('xfdf')
 
 
 /** Display the available fields of a PDF form
@@ -45,11 +45,50 @@ function fields (pdf) {
 }
 
 
+const pdfDate = (date) => {
+  if (!date) return ''
+  if (typeof date === 'string') return date
+  return 'D:' + date.toISOString().replace(/[-:T]/g, '').replace(/\..*/, "Z00'00'")
+}
+
+const pdfInfo = (info) => {
+  if (info.ModDate) info.ModDate = pdfDate(info.ModDate)
+  if (info.CreationDate) info.CreationDate = pdfDate(info.CreationDate)
+  return Object.keys(info).reduce((str, key) => (
+    str + `InfoBegin\nInfoKey: ${key}\nInfoValue: ${info[key] || ''}\n`
+  ), '')
+}
+
+const setInfo = (pdf, info, reject) => {
+  const { stdin, stdout, stderr } = spawn('pdftk', [pdf, 'update_info_utf8', '-', 'output', '-'])
+  stderr.on('data', reject)
+  stdin.write(pdfInfo(info), 'utf8', () => stdin.end())
+  return stdout
+}
+
+const fillForm = (input, xfdf, flatten, reject, resolve) => {
+  const args = ['-', 'fill_form', xfdf, 'output', '-']
+  if (flatten) args.push('flatten')
+  const { stdin, stdout, stderr } = spawn('pdftk', args, { stdio: [input] })
+  input.destroy()  // https://github.com/nodejs/node/issues/9413#issuecomment-258604006
+  const listener = (data) => {
+    stdout.pause()
+    stdout.unshift(data)
+    stdout.removeListener('data', listener)
+    resolve(stdout)
+  }
+  stderr.on('data', reject)
+  stdout.on('data', listener)
+}
+
+
 /** Fill a PDF form with data
  *
  * @param {string} pdf - path for the source PDF file
- * @param {Object.<string,string>} fields - a flat map of data to populate the form's fields
- * @param {?Object} options - customise the output; currently only supports `flatten`, default true
+ * @param {Object} fields - a flat map of data to populate the form's fields
+ * @param {Object} [options] - optionally customise the output
+ * @param {boolean} [options.flatten=true] - Flatten the resulting PDF
+ * @param {Object} [options.info] - info fields to be set in the output
  * @returns {Promise(stream.Readable)} provides the output PDF
  *
  * @example
@@ -64,28 +103,23 @@ function fields (pdf) {
  *   .catch(err => console.error(err))
  */
 function fill (pdf, fields, options = {}) {
-  const { flatten = true } = options
-  const xfdfBuilder = new xfdf({ pdf })
+  const { flatten = true, info } = options
+  const xfdfBuilder = new XFDF({ pdf })
   xfdfBuilder.fromJSON({ fields })
   return new Promise((resolve, reject) => {
+    const _reject = (err) => {
+      if (!(err instanceof Error)) err = new Error(err.toString())
+      reject(err)
+    }
     fs.access(pdf, fs.constants.R_OK, (err) => {
-      if (err) return reject(err)
-      tmp.file((err, path, fd) => {
-        if (err) return reject(err)
+      if (err) return _reject(err)
+      tmp.file((err, xfdf, fd) => {
+        if (err) return _reject(err)
         fs.write(fd, xfdfBuilder.generate(), (err, written) => {
-          if (err) return reject(err)
-          if (written === 0) return reject(new Error('xfdf wrote 0 bytes!'))
-          const args = [pdf, 'fill_form', path, 'output', '-']
-          if (flatten) args.push('flatten')
-          const { stderr, stdout } = spawn('pdftk', args)
-          const listener = (data) => {
-            stdout.pause()
-            stdout.unshift(data)
-            resolve(stdout)
-            stdout.removeListener('data', listener)
-          }
-          stderr.on('data', (err) => reject(new Error(err.toString())))
-          stdout.on('data', listener)
+          if (err) return _reject(err)
+          if (written === 0) return _reject('xfdf wrote 0 bytes!')
+          const stream = setInfo(pdf, info, _reject)
+          fillForm(stream, xfdf, flatten, _reject, resolve)
         })
       })
     })
